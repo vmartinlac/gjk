f173b1739a65ae06c6fd7705910fa742a567dd10
Victor Martin Lac <victornement@free.fr>
2018-03-31 00:08:09 +0200

    Work in progress.

--- TODO.md
+++ TODO.md
@@ -1,4 +1,5 @@
 * calculer point de contact lors de collision.
+* améliorer implantation GJK.
 * comment recenser les forces qui s'exercent sur un objet ?
 * comment traiter les collisions.
 * tester la résolution des équations d'Euler.
--- src/main.cpp
+++ src/main.cpp
@@ -31,39 +31,42 @@ int main(int num_args, char** args)
 
     Solver* solver = new Solver;
 
-    BodyPtr body1( new SphereBody(1.0, CTE_WOOD_DENSITY) );
-    body1->initialState().position << -3.0, 0.0, 3.0;
-    body1->setFixed(false);
-    solver->addBody(body1);
+    {
+        BodyPtr body1( new SphereBody(1.0, CTE_WOOD_DENSITY) );
+        body1->initialState().position << -3.0, 0.0, 3.0;
+        body1->setFixed(false);
+        solver->addBody(body1);
 
-    BodyPtr body2( new SphereBody(1.0, CTE_WOOD_DENSITY) );
-    body2->initialState().position << -3.0, 0.0, 10.0;
-    body2->setFixed(false);
-    solver->addBody(body2);
+        BodyPtr body2( new SphereBody(1.0, CTE_WOOD_DENSITY) );
+        body2->initialState().position << -3.0, 0.0, 10.0;
+        body2->setFixed(false);
+        solver->addBody(body2);
 
-    BodyPtr body3( new BoxBody( Eigen::Vector3d{2.0, 2.0, 2.0}, CTE_IRON_DENSITY ) );
-    body3->initialState().position << 3.0, 0.0, 10.0;
-    body3->setFixed(false);
-    solver->addBody(body3);
+        BodyPtr body3( new BoxBody( Eigen::Vector3d{2.0, 2.0, 2.0}, CTE_WOOD_DENSITY ) );
+        body3->initialState().position << 3.0, 0.0, 10.0;
+        body3->setFixed(false);
+        solver->addBody(body3);
 
-    BodyPtr body5( new BoxBody( Eigen::Vector3d{2.0, 2.0, 2.0}, CTE_IRON_DENSITY ) );
-    body5->initialState().position << 3.0, 0.0, 20.0;
-    body5->initialState().attitude.setFromTwoVectors(
-        Eigen::Vector3d(0.0, 0.0, 1.0),
-        Eigen::Vector3d(1.4, 1.0, 1.0)
-    );
-    body5->initialState().angular_momentum << 200000.0, 0.0, 0.0;
-    body5->setFixed(false);
-    solver->addBody(body5);
+        BodyPtr body5( new BoxBody( Eigen::Vector3d{2.0, 2.0, 2.0}, CTE_WOOD_DENSITY ) );
+        body5->initialState().position << 3.0, 0.0, 20.0;
+        body5->initialState().attitude.setFromTwoVectors(
+            Eigen::Vector3d(0.0, 0.0, 1.0),
+            Eigen::Vector3d(1.4, 1.0, 1.0)
+        );
+        body5->initialState().linear_momentum << 0.0, 0.0, 100000.0;
+        body5->initialState().angular_momentum << 20000.0, 0.0, 0.0;
+        body5->setFixed(false);
+        solver->addBody(body5);
 
-    BodyPtr body4( new BoxBody( Eigen::Vector3d{20.0, 20.0, 1.0}, CTE_IRON_DENSITY ) );
-    body4->initialState().position << 0.0, 0.0, -6.0;
-    body4->initialState().attitude.setFromTwoVectors(
-        Eigen::Vector3d(0.0, 0.0, 1.0),
-        //Eigen::Vector3d(0.4, -0.4, 0.7)
-        Eigen::Vector3d(0.4, 0.0, 0.7)
-    );
-    solver->addBody(body4);
+        BodyPtr body4( new BoxBody( Eigen::Vector3d{20.0, 20.0, 1.0}, CTE_IRON_DENSITY ) );
+        body4->initialState().position << 0.0, 0.0, -6.0;
+        body4->initialState().attitude.setFromTwoVectors(
+            Eigen::Vector3d(0.0, 0.0, 1.0),
+            //Eigen::Vector3d(0.4, -0.4, 0.7)
+            Eigen::Vector3d(0.4, 0.0, 0.7)
+        );
+        solver->addBody(body4);
+    }
 
     solver->init();
 
